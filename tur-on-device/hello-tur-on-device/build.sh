1bfffcbf018f87c99157dfe3475d18e29ef7f560
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-03-09 16:43:52 +0800

    [Do Not Merge]: test C++ build

--- tur-on-device/hello-tur-on-device/build.sh
+++ tur-on-device/hello-tur-on-device/build.sh
@@ -3,11 +3,12 @@ TERMUX_PKG_DESCRIPTION="Dummy test for TUR on Device"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=0.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
-	$CC $CFLAGS $CPPFLAGS $TERMUX_PKG_BUILDER_DIR/main.c -o hello-tur-on-device
+	$CXX $CXXFLAGS $CPPFLAGS $TERMUX_PKG_BUILDER_DIR/main.c -o hello-tur-on-device
 }
 
 termux_step_make_install() {
